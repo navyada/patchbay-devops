625763c3f29389a21226c8669280394d45c2627e
navyada <koshatwa@usc.edu>
2023-11-21 22:38:49 +0530

    fixed bucket name issue again

--- app/app/settings.py
+++ app/app/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 
-STATIC_ROOT = 'vol/web/static'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
